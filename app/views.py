322b5b48b1e3b683ab219e211cb4459e9c501868
robertobzrr <robertobezerrafh@outlook.com>
2025-04-17 13:10:02 -0300

    render data into HTML

--- app/views.py
+++ app/views.py
@@ -3,10 +3,34 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, "index.html")
+
+
+    list_itens = [
+
+        {
+            "id" : "01",
+            "name" : "Beef",
+            "category" : "Meat",
+        },
+
+        {
+            "id" : "02",
+            "name" : "Pork",
+            "category" : "Meat",
+        }
+
+    ]
+    context = {"list": list_itens}
+    #name = "roberto"
+    #context = {"first_name": name}
+    return render(request, "index.html", context=context) #, context=context (dentro do ()))
+
+
 
 def categoriesPage(request):
     return render(request, "categoriesPage.html")
+
+
 
 def listPage(request):
     return render(request, "listPage.html")
